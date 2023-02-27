00ec51e234028a2f3013ec1c09fef9727a375fdd
Chloe Choe <chloe410993@lyron.gusd.lcl>
2023-02-27 21:45:48 +0000

    Add existing file

---
+++ pw5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Random;
+
+public class pw5{
+        public static void main(String args[]){
+                Scanner sc = new Scanner(System.in);
+                Random rand = new Random();
+		//array
+		String password = 
+                System.out.println("Enter  a password:");
+		String guess = sc.nextLine();
+
+        }
+}
